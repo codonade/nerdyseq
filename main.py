9ce0451003fd3ca725cbedf215ec7f6198785efe
codonade <abdulrahman.haytham.shaddad@hotmail.com>
2024-02-29 17:24:38 +0300

    handled `fibonacci` sequences

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from enum import Enum
 class Sequence(Enum):
   ARITHMETIC = 0
   GEOMETRIC = 1
+  FIBONACCI = 2
   UNKNOWN = -1
 
 # TODO: Switch to floats instead of integers.
@@ -16,7 +17,7 @@ def identify_sequence(sequence: list[int]) -> Sequence:
   # NOTE: This difference should apply for the whole sequence.
   is_arithmetic =\
     all(sequence[i] - sequence[i-1] == arithmetic_diff\
-        for i in range(2, len(sequence)))
+      for i in range(2, len(sequence)))
 
   # ~ Geometric Sequence Check.
   if sequence[0] != 0:
@@ -24,14 +25,24 @@ def identify_sequence(sequence: list[int]) -> Sequence:
     # NOTE: This ratio should apply for the whole sequence.
     is_geometric =\
       all(sequence[i] / sequence[i-1] == geometric_ratio\
-          for i in range(2, len(sequence)))
+        for i in range(2, len(sequence)))
   else:
     is_geometric = False
+
+  # Fibonacci sequence check
+  is_fibonacci = True
+  for i in range(2, len(sequence)):
+    if sequence[i-1] + sequence[i-2] != sequence[i]:
+      # NOTE: The current number should be a sum of the preceding two.
+      is_fibonacci = False
+      break
 
   if is_arithmetic:
     return Sequence.ARITHMETIC
   elif is_geometric:
     return Sequence.GEOMETRIC
+  elif is_fibonacci:
+    return Sequence.FIBONACCI
   else:
     print(f"Couldn't identify the sequence."); exit(0)
 
@@ -48,6 +59,20 @@ def compute_snth_term(sequence: list[int], n: int) -> int:
       a = sequence[0]
       r = int(sequence[1] / a)
       return a * (r ** (n - 1))
+    case Sequence.FIBONACCI:
+      if n == 1:
+        return sequence[0]
+      elif n == 2:
+        return sequence[1]
+      else:
+        i, c = 0, 0
+        # NOTE: Typically 0 and 1.
+        a, b = sequence[0], sequence[1]
+        while i < n:
+          c = a + b
+          a, b = b, c
+          i += 1
+        return c
     case Sequence.UNKNOWN:
       # NOTE: Case already handled by `identify_sequence`
       return 0
